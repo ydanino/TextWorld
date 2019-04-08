7f57cc60be636ecfd2710c90f0f24921a9bdfc46
ydanino999 <ydanino999@student.fuhsd.org>
2019-04-08 10:05:11 -0700

    Graph

--- src/Graph.java
+++ src/Graph.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class Graph {
 
     private HashMap<String, Node> nodes;
-
+    private ArrayList<Animals> allAnimals;
+    private Player p;
 
     public Graph(){
         nodes = new HashMap<String, Node>();
+        allAnimals = new ArrayList<Animals>();
     }
 
     public void addNode(String name, String description) {
@@ -36,18 +38,78 @@ public class Graph {
 
     }
 
+    public void addAnimal(Animals a){
+        allAnimals.add(a);
+    }
 
+    public Animals removeAnimal(String name){
+        for (int i = 0; i < allAnimals.size() ; i++) {
+            if(allAnimals.get(i).getName().equals(name)){
+
+                return allAnimals.remove(i);
+            }
+        }
+        return null;
+    }
+
+    public void removeAnimal(Animals a){
+        if(!allAnimals.contains(a))return;
+
+        allAnimals.remove(a);
+    }
+
+    public void updateAllAnimals(){
+        for (Animals a : allAnimals) {
+            System.out.println("all animals size" + allAnimals.size());
+            a.move();
+        }
+    }
+
+    public String displayAnimals(Node room){
+        String animalNames = "";
+        for(Animals a : allAnimals){
+            if(a.getCurrentRoom().equals(room)){
+                animalNames += a.getName() + " ";
+            }
+        }
+        return animalNames;
+    }
 
     public class Node{
         private String name; // room name
         private HashMap<String, Node> neighbors;
         private String description;
+        public ArrayList<Item> roomItems;
+        public ArrayList<Animals> aninmals;
 
         private Node(String name, String description) {
             neighbors = new HashMap<String, Node>();
             this.name = name;
             this.description = description;
+            this.roomItems = new ArrayList<Item>();
         }
+
+
+
+        public String getItems(){
+            String itemNames = "";
+            for (int i = 0; i < roomItems.size(); i++) {
+                itemNames += roomItems.get(i).getName() + " ";
+            }
+            return itemNames;
+        }
+
+        public void addItem(Item i){
+            roomItems.add(i);
+        }
+
+        public void removeItem(Item i){
+            if(!roomItems.contains(i))return;
+
+            roomItems.remove(i);
+
+        }
+
         public String getDescrip(){
             return description;
         }
@@ -66,18 +128,31 @@ public class Graph {
         public String getNeighborNames(){
             String names = "";
             for (String n : neighbors.keySet()) {
-                names += neighbors.get(n);
+                names += neighbors.get(n).getName() + " ";
             }
             return names;
         }
+
+        public ArrayList<Node> getNeighnorNamesArrayList(){
+            ArrayList<Node> neighborNames = new ArrayList<Node>();
+
+            for( String n: neighbors.keySet()){
+                neighborNames.add(neighbors.get(n));
+            }
+
+            return neighborNames;
+        }
+
+        public HashMap getNeighbors(){
+            return neighbors;
+        }
+
 
 
         public Node getNeighbor(String name){
             return neighbors.get(name);
 
         }
-
-
 
         public String getName(){
             return name;
